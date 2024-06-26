0a729739c9b9fc00547844b2ec3ed2fa135f8112
ThanhHao <Acer@DESKTOP-8MK870A>
2024-06-26 22:07:21 +0700

    HAOOOO

---
+++ Dat_Do_An.Module/Controllers/BAOHANH.cs
@@ -0,0 +1,77 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Dat_Do_An.Module.Controllers
+{
+    [DefaultClassOptions]
+    //[ImageName("BO_Contact")]
+    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    [DefaultProperty("ThoiGianBH")]
+    [DefaultListViewOptions(MasterDetailMode.ListViewOnly, true, NewItemRowPosition.Top)]
+    //[Persistent("DatabaseTableName")]
+    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+    public class BAOHANH : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        // Use CodeRush to create XPO classes and properties with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/118557
+        public BAOHANH(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+        //private string _PersistentProperty;
+        //[XafDisplayName("My display name"), ToolTip("My hint message")]
+        //[ModelDefault("EditMask", "(000)-00"), Index(0), VisibleInListView(false)]
+        //[Persistent("DatabaseColumnName"), RuleRequiredField(DefaultContexts.Save)]
+        //public string PersistentProperty {
+        //    get { return _PersistentProperty; }
+        //    set { SetPropertyValue(nameof(PersistentProperty), ref _PersistentProperty, value); }
+        //}
+
+        //[Action(Caption = "My UI Action", ConfirmationMessage = "Are you sure?", ImageName = "Attention", AutoCommit = true)]
+        //public void ActionMethod() {
+        //    // Trigger a custom business logic for the current record in the UI (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112619.aspx).
+        //    this.PersistentProperty = "Paid";
+        //}
+        private string _ThoiGianBH;
+        [Size(50), XafDisplayName("Thoi Gian Bao Hanh")]
+        public string ThoiGianBH
+        {
+            get { return _ThoiGianBH; }
+            set { SetPropertyValue<string>(nameof(ThoiGianBH), ref _ThoiGianBH, value); }
+        }
+
+
+        private int _SoluongBH;
+        [Size(50), XafDisplayName("So Luong Bao Hanh")]
+        public int SoluongBH
+        {
+            get { return _SoluongBH; }
+            set { SetPropertyValue<int>(nameof(SoluongBH), ref _SoluongBH, value); }
+        }
+
+
+
+        [DevExpress.Xpo.Aggregated, Association("KEY_BH_DX")]
+        public XPCollection<DONGXUAT> _DONGXUAT
+        {
+            get { return GetCollection<DONGXUAT>(nameof(_DONGXUAT)); }
+        }
+    }
+}
